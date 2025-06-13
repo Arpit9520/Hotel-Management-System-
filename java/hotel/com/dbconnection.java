74ecc894d47c850deb13daafcdb3f4e92c5785f5
Arpit9520 <arpitprajapati947@gmail.com>
2025-06-13 14:05:39 +0530

    Update dbconnection.java

--- java/hotel/com/dbconnection.java
+++ java/hotel/com/dbconnection.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class dbconnection {
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotelmanagement","root","Arpit@95");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotelmanagement","root","Arpit@123");
 
 		}
 		return conn;
